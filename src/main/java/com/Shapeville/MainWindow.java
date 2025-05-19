274bfa0625cf19ad5d5f42efbb0139466cbde1d1
LiXiaoYaoCareFree <2210127151@qq.com>
2025-05-19 20:47:40 +0800

    Set the English format

--- src/main/java/com/Shapeville/MainWindow.java
+++ src/main/java/com/Shapeville/MainWindow.java
@@ -1,12 +1,14 @@
 package com.Shapeville;
 
 import javax.swing.*;
+import java.util.Locale;
 
 public class MainWindow {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                Locale.setDefault(Locale.ENGLISH);
                 ShapevilleGUI frame = new ShapevilleGUI();
                 frame.setVisible(true);
             }
